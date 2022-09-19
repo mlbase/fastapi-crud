624e5b1897ac3b3759cff9ec7fbea3774c384ae5
mlbase <guddnr2424@gmail.com>
2022-09-19 17:28:59 +0900

    ddl

---
+++ ddl.sql
@@ -0,0 +1,16 @@
+create table api_user(
+    id INTEGER primary key auto_increment,
+    full_name varchar(100),
+    email text,
+    hashed_password text,
+    is_active boolean
+)
+
+create table item(
+    id INTEGER primary key ,
+    title VARCHAR(100),
+    description text,
+    owner_id INTEGER
+);
+alter table item
+add foreign key (owner_id) references api_user(id)
