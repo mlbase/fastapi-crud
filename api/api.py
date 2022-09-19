955e3bf132b1dd33ce5c4720a7ca2ba3844fbe32
mlbase <guddnr2424@gmail.com>
2022-09-19 17:15:53 +0900

    api router path fix

--- api/api.py
+++ api/api.py
@@ -3,4 +3,4 @@ from fastapi import APIRouter
 from api.v1 import user_api
 
 api_router = APIRouter()
-api_router.include_router(user_api.router, prefix="/user.py", tags=["users"])
+api_router.include_router(user_api.router, prefix="/users", tags=["users"])
