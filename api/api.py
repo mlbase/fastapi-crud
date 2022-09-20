314f6c3af8de74110836f5b08a72dfd804b4bc71
mlbase <guddnr2424@gmail.com>
2022-09-20 18:03:37 +0900

    api router 수정

--- api/api.py
+++ api/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from api.v1 import user_api
+from api.v1 import user_api, login_api
 
 api_router = APIRouter()
 api_router.include_router(user_api.router, prefix="/users", tags=["users"])
+api_router.include_router(login_api.router, prefix="/logins", tags=["logins"])
