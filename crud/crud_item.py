13901da988811dd51474fdebcd553587cb3db55c
mlbase <guddnr2424@gmail.com>
2022-09-26 18:31:11 +0900

    비동기 처리를 위한 crud(DAO) 변경

--- crud/crud_item.py
+++ crud/crud_item.py
@@ -1,29 +1,28 @@
 from typing import List
 
 from fastapi.encoders import jsonable_encoder
-from sqlalchemy.orm import Session
-
+from sqlalchemy.ext.asyncio import AsyncSession
 from crud.base import CRUDBase
 from model.item import Item
 from schema.item import ItemCreate, ItemUpdate
 
 
 class CRUDItem(CRUDBase[Item, ItemCreate, ItemUpdate]):
-    def create_with_owner(
-        self, db: Session, *, obj_in: ItemCreate, owner_id: int
+    async def create_with_owner(
+        self, db: AsyncSession, *, obj_in: ItemCreate, owner_id: int
     ) -> Item:
         obj_in_data = jsonable_encoder(obj_in)
         db_obj = self.model(**obj_in_data, owner_id=owner_id)
         db.add(db_obj)
         db.commit()
         db.refresh(db_obj)
-        return db_obj
+        return await db_obj
 
-    def get_multi_by_owner(
-        self, db: Session, *, owner_id: str, skip: int = 0, limit: int = 100
+    async def get_multi_by_owner(
+        self, db: AsyncSession, *, owner_id: str, skip: int = 0, limit: int = 100
     ) -> List[Item]:
-        return (
-            db.query(self.model)
+        return await (
+            db.get(self.model)
             .filter(Item.owner_id == owner_id)
             .offset(skip)
             .limit(limit)
--- crud/crud_user.py
+++ crud/crud_user.py
@@ -1,30 +1,35 @@
 from typing import Any, Dict, Optional, Union
 
-from sqlalchemy.orm import Session
-
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 from config.security import get_password_hash, verify_password
 from crud.base import CRUDBase
 from model.user import User
 from schema.user import UserCreate, UserUpdate
+from sqlalchemy.exc import NoResultFound
 
 
 class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
-    async def get_by_email(self, session: Session, *, email: str) -> User | None:
-        return session.query(User).filter(User.email == email).first()
+    async def get_by_email(self, session: AsyncSession, *, email: str) -> User | None:
+        statement = select(User).where(User.email == email)
 
-    def create(self, session: Session, *, obj_in: UserCreate) -> User:
+        q_object = await session.execute(statement=statement, params={"email_1": email})
+        return q_object.scalars().first()
+
+    async def create(self, session: AsyncSession, *, obj_in: UserCreate) -> User:
         db_obj = User(
             email=obj_in.email,
             hashed_password=get_password_hash(obj_in.password),
             full_name=obj_in.full_name
         )
+
         session.add(db_obj)
-        session.commit()
-        session.refresh(db_obj)
+        await session.commit()
+        await session.refresh(db_obj)
         return db_obj
 
-    def update(
-            self, session: Session, *,db_obj: User, obj_in: Union[UserUpdate, Dict[str, Any]]
+    async def update(
+            self, session: AsyncSession, *,db_obj: User, obj_in: Union[UserUpdate, Dict[str, Any]]
     ) -> User:
         if isinstance(obj_in, dict):
             update_data = obj_in
@@ -34,19 +39,21 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
             hashed_password = get_password_hash(update_data["password"])
             del update_data["paasword"]
             update_data["hashed_password"] = hashed_password
-        return super().update(session=session, db_obj=db_obj, obj_in=update_data)
+        return await super().update(session=session, db_obj=db_obj, obj_in=update_data)
 
-    def authenticate(self, db: Session, *, email: str, password: str) -> User | None:
+    async def authenticate(self, db: AsyncSession, *, email: str, password: str) -> User | None:
         user = self.get_by_email(db, email=email)
         print(user)
         if not user:
             return None
         if not verify_password(password, user.hashed_password):
             return None
-        return user
+        return await user
 
-    def is_active(self, user: User) -> bool:
-        return user.is_active
+    async def is_active(self, user: User) -> bool:
+        return await user.is_active
 
 
 user = CRUDUser(User)
+
+
