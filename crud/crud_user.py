d916e5e8849376cce78f0b0064cc4f3f8c6fd159
mlbase <guddnr2424@gmail.com>
2022-09-28 18:35:50 +0900

    row query로 relation mapping 하는 crud 추가

--- crud/crud_user.py
+++ crud/crud_user.py
@@ -2,10 +2,13 @@ from typing import Any, Dict, Optional, Union
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+from sqlalchemy.orm import selectinload
+
+import model
 from config.security import get_password_hash, verify_password
 from crud.base import CRUDBase
 from model.user import User
-from schema.user import UserCreate, UserUpdate
+from schema.user import UserCreate, UserUpdate, UserWithItem
 from sqlalchemy.exc import NoResultFound
 
 
@@ -29,7 +32,7 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
         return db_obj
 
     async def update(
-            self, session: AsyncSession, *,db_obj: User, obj_in: Union[UserUpdate, Dict[str, Any]]
+            self, session: AsyncSession, *, db_obj: User, obj_in: Union[UserUpdate, Dict[str, Any]]
     ) -> User:
         if isinstance(obj_in, dict):
             update_data = obj_in
@@ -53,7 +56,31 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
     async def is_active(self, user: User) -> bool:
         return await user.is_active
 
+    async def get_by_id(self, session: AsyncSession, id: int) -> None:
+        statement = select(model.User).options(selectinload(model.User.items))\
+            .filter(model.User.id == id)
+
+        q_object = await session.execute(statement=statement)
+        result = q_object.scalar()
+        return result
+
+    async def get_by_id_raw(self, session: AsyncSession, id: int) -> None:
+        statement1 = "SELECT api_user.* " \
+                     "FROM api_user " \
+                     "WHERE api_user.id = :id_1"
+        statement2 = "SELECT item.* " \
+                     "FROM item " \
+                     "WHERE item.owner_id = :id_2"
+
+        q_object1 = await session.execute(statement=statement1, params={"id_1": id})
+        q_object2 = await session.execute(statement=statement2, params={"id_2": id})
+        user_data = q_object1.first()
+        print(user_data)
+        item_data = q_object2.all()
+        print(item_data)
+        print(user_data)
+        print(item_data)
+        result = UserWithItem(**user_data, items=item_data)
+        return result
 
 user = CRUDUser(User)
-
-
