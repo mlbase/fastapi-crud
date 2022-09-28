d47911177fbab37fbcf36024193facd5267a2c0c
mlbase <guddnr2424@gmail.com>
2022-09-28 18:36:56 +0900

    relationship 가져오는 api 추가

--- api/v1/login_api.py
+++ api/v1/login_api.py
@@ -1,13 +1,16 @@
+# pip dependency
 from datetime import timedelta
 from typing import Any
-from pydantic import BaseModel
 from fastapi import APIRouter, Body, Depends, HTTPException
+from fastapi.encoders import jsonable_encoder, ENCODERS_BY_TYPE
 from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 import databases
-from sqlalchemy.sql import table, select
-import model
+import logging
+import json
+# local dependency
 import crud
+from crud.crud_user import user
 import model
 import schema
 from utils import dependencies
@@ -15,7 +18,6 @@ from config import security
 from config.security import get_password_hash
 from config.setting import settings
 from config.session_factory import engine, SQLALCHEMY_DATABASE_URL
-import logging
 
 router = APIRouter()
 
@@ -24,25 +26,25 @@ logging.basicConfig()
 logging.getLogger("database.Database").setLevel(logging.INFO)
 
 
-@router.post("/access-token",
+@router.post("/access_token",
              response_model=schema.Token,
              responses={
                  200: {
-                        "model": schema.Token,
-                        "description": "jwt response successfully",
-                        "content": {
-                            "application/json": {
-                                "example": {
-                                    "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHA...",
-                                    "token_type": "Bearer"
-                                },
-                                "schema": {
-                                    "access_token": "jwt 토큰",
-                                    "token_type": "베어러 타입"
-                                }
-                            }
-                        }
-                       },
+                     "model": schema.Token,
+                     "description": "jwt response successfully",
+                     "content": {
+                         "application/json": {
+                             "example": {
+                                 "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHA...",
+                                 "token_type": "Bearer"
+                             },
+                             "schema": {
+                                 "access_token": "jwt 토큰",
+                                 "token_type": "베어러 타입"
+                             }
+                         }
+                     }
+                 },
                  400: {"description": "Incorrect email or password"}
              },
              summary="토큰발급 api"
@@ -58,35 +60,78 @@ async def login_access_token(
             WHERE api_user.email = :email
             """
     # query = select(model.User).filter(model.User.email)
-    # print(query)
+    print(query)
     try:
         await database.connect()
         print("connection start")
-        user = await database.fetch_one(query=query, values={"email": form_data.username})
-        print(user)
-    except ConnectionRefusedError:
+        print(form_data.username)
+        current_user = await database.fetch_one(query=query, values={"email": form_data.username})
+        if not current_user:
+            raise HTTPException(status_code=400, detail="Incorrect email or password")
+
+    except Exception:
         raise HTTPException(status_code=503, detail="Too many Request")
     finally:
         print("fetching end...")
         await database.disconnect()
     print("connection end")
-    password_check = security.verify_password(plain_password=form_data.password, hashed_password=user.hashed_password)
-    if not user:
+    password_check = security.verify_password(plain_password=form_data.password,
+                                              hashed_password=current_user.hashed_password)
+    if not password_check:
         raise HTTPException(status_code=400, detail="Incorrect email or password")
-    elif not password_check:
-        raise HTTPException(status_code=400, detail="Incorrect email or password")
-    elif not crud.user.is_active(user):
+    elif not crud.user.is_active(current_user):
         raise HTTPException(status_code=400, detail="Inactive user")
 
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     return {
         "access_token": security.create_access_token(
-            subject=user.id, expires_delta=access_token_expires, superuser_check=user.is_superuser
+            subject=current_user.id, expires_delta=access_token_expires, superuser_check=current_user.is_superuser
         ),
         "token_type": "bearer",
     }
 
 
-@router.post("/test-token", response_model=schema.User)
+@router.post("/test_token", response_model=schema.User)
 def test_token(current_user: model.User = Depends(dependencies.get_current_user)) -> Any:
     return current_user
+
+
+@router.get("/",
+            response_model=schema.UserWithItem,
+            responses={
+                200: {
+                    "model": schema.UserWithItem,
+                    "description": "my page response successfully",
+                    "content": {
+                        "application/json": {
+                            "example": {
+                                "email": "user@example.com",
+                                "is_active": True,
+                                "full_name": "example name",
+                                "items": [
+                                    {
+                                        "title": "item title",
+                                        "description": "this item is..."
+                                    }
+                                ]
+                            },
+                            "schema": {
+                                "email": "user의 email",
+                                "is_active": "활성화 되어 있는지 여부 boolean",
+                                "full_name": "user의 이름",
+                                "items": "item에 대한 리스트"
+                            }
+                        }
+                    }
+                }
+            }
+            )
+async def user_my_page(
+        current_user: model.User = Depends(dependencies.get_current_user),
+        session: AsyncSession = Depends(dependencies.get_db)
+) -> Any:
+    current_id = current_user.id
+    result = await user.get_by_id_raw(session, current_id)
+    # print(response)
+
+    return result
