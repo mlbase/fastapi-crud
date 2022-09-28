59b8b7301ab82aeb83fa841cb15d38ca95cb3a53
mlbase <guddnr2424@gmail.com>
2022-09-28 18:35:23 +0900

    crud에 비동기 처리 추가

--- crud/crud_item.py
+++ crud/crud_item.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Any
 
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -10,13 +10,13 @@ from schema.item import ItemCreate, ItemUpdate
 class CRUDItem(CRUDBase[Item, ItemCreate, ItemUpdate]):
     async def create_with_owner(
         self, db: AsyncSession, *, obj_in: ItemCreate, owner_id: int
-    ) -> Item:
+    ) -> Any:
         obj_in_data = jsonable_encoder(obj_in)
         db_obj = self.model(**obj_in_data, owner_id=owner_id)
         db.add(db_obj)
-        db.commit()
-        db.refresh(db_obj)
-        return await db_obj
+        await db.commit()
+        await db.refresh(db_obj)
+        return db_obj
 
     async def get_multi_by_owner(
         self, db: AsyncSession, *, owner_id: str, skip: int = 0, limit: int = 100
